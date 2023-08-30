8e70f44f39e93f5b643dbfdd29d9b1e47b8b9204
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-08-30 20:57:49 +0500

    completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className='gpt3__navbar'>
+    <div className='gpt3__navbar' id='top'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
